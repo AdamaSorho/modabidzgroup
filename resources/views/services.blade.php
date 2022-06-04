5febafcc519564d34f484dd1c14bf5ed18cb1ccc
Adama Sorho <djomantcha@gmail.com>
2022-06-04 18:17:37 +0000

    added service image

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -22,7 +22,13 @@
                     <div class="col-lg-3 col-md-6 pb-1">
                         <div class="d-flex flex-column align-items-center justify-content-center text-center bg-light mb-4 px-4" style="height: 300px;">
                             <div class="d-inline-flex align-items-center justify-content-center bg-white shadow rounded-circle mb-4" style="width: 100px; height: 100px;">
-                                <i class="fa fa-3x fa-cloud-sun text-secondary"></i>
+                                @if($service->image)
+                                    <a href="{{ url($service->image) }}">
+                                        <img src="{{ url($service->image) }}" style="border-radius: 50%;" width="100" height="100" alt="{{ $service->wording }}">
+                                    </a>
+                                @else
+                                    <i class="fa fa-3x fa-cloud-sun text-secondary"></i>
+                                @endif
                             </div>
                             <h4 class="font-weight-bold m-0">{{ $service->wording }}</h4>
                         </div>
